7672d644862a8ff0030ed8a9c5ddee7646ec59ec
Tyler Greer <tgreer812@gmail.com>
2023-08-26 18:43:17 -0400

    Create updatescripts.lua

---
+++ updatescripts.lua
@@ -0,0 +1,30 @@
+-- Table containing the raw GitHub URLs of the scripts to update
+local githubURLs = {
+    "https://raw.githubusercontent.com/tgreer812/computercraftscripts/main/minerscript.lua",
+    "https://raw.githubusercontent.com/YourUsername/YourRepo/main/Script2.lua",
+    -- Add more URLs as needed
+}
+
+-- Function to extract the script name from a GitHub URL
+local function extractScriptName(url)
+    local _, _, scriptName = string.find(url, "/([^/]+)$")
+    return scriptName or "unknown.lua"
+end
+
+-- Function to update a single script
+local function updateScript(url)
+    local scriptName = extractScriptName(url)
+    print("Updating " .. scriptName .. "...")
+
+    local ok, err = shell.run("wget " .. url .. " " .. scriptName)
+    if not ok then
+        print("Failed to update " .. scriptName .. ": " .. tostring(err))
+    end
+end
+
+-- Main loop to update all scripts
+for _, url in ipairs(githubURLs) do
+    updateScript(url)
+end
+
+print("Update complete.")
