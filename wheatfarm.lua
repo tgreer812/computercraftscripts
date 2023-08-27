db1e94b2d44ee21be0a8c68bf17e0df0b311fdb6
John Mitchell <jhmitchell@uchicago.edu>
2023-08-26 23:37:43 -0500

    updated wheat farm

--- wheatfarm.lua
+++ wheatfarm.lua
@@ -1,41 +1,69 @@
--- Farming Script for CC:Tweaked Turtle in 
--- an Arbitrarily Large Wheat Field surrounded
--- by a Cobblestone Wall
+-- Constants for fuel
+local FUEL_THRESHOLD = 10
+local REFUEL_SLOT = 16
 
+-- Function to check and refuel the turtle
+function checkFuel()
+    if turtle.getFuelLevel() < FUEL_THRESHOLD then
+        print("Low fuel! Attempting to refuel...")
+        turtle.select(REFUEL_SLOT)
+        while turtle.getFuelLevel() < FUEL_THRESHOLD do
+            turtle.refuel(1)
+            os.sleep(10)
+        end
+        print("Refueled!")
+    end
+end
+
+-- Inspect and take action based on the block below the turtle
 function inspect()
+    -- Check fuel before any action
+    checkFuel()
+
     local success, data = turtle.inspectDown()
 
-    -- If the block is wheat, check if it's fully grown
     if success and data.name == "minecraft:wheat" then
-        print("harvesting...")
+        print("Harvesting...")
         if data.metadata == 7 then
             turtle.digDown()
         end
         turtle.select(1)
         turtle.placeDown()
+
         return
     end
 
-    -- Otherwise, go down and check if it's dirt
     turtle.down()
     success, data = turtle.inspectDown()
-    if success and data.name == "minecraft:dirt" or data.name == "minecraft:grass_block" then
-        print("tilling...")
+    if success and data.name == "minecraft:farmland" then
+        print("Planting on tilled land...")
+        turtle.up()
+        turtle.select(1)
+        turtle.placeDown()
+        return
+    elseif success and (data.name == "minecraft:dirt" or data.name == "minecraft:grass_block") then
+        print("Tilling and planting...")
+        turtle.up()
         turtle.digDown()
         turtle.select(1)
         turtle.placeDown()
+        return
+    else
+        turtle.up()
     end
-    turtle.up()
 end
 
+-- Move the turtle based on its mode
 function move(mode)
+    -- Check fuel before moving
+    checkFuel()
+
     local success, data = turtle.inspect()
     
     if success and data.name == "minecraft:cobblestone" then
         if mode == "forward" then
             print("Turning right from forward mode")
             turtle.turnRight()
-            
             success, data = turtle.inspect()
             if success and data.name == "minecraft:cobblestone" then
                 turtle.turnRight()
@@ -46,11 +74,9 @@ function move(mode)
                 turtle.turnRight()
                 return "backward"
             end
-
         elseif mode == "backward" then
             print("Turning left from backward mode")
             turtle.turnLeft()
-            
             success, data = turtle.inspect()
             if success and data.name == "minecraft:cobblestone" then
                 turtle.turnRight()
@@ -61,15 +87,13 @@ function move(mode)
                 turtle.turnLeft()
                 return "forward"
             end
-
         elseif mode == "returning" then
             print("Returning to start")
-            
             success, data = turtle.inspect()
             if success and data.name == "minecraft:cobblestone" then
                 turtle.turnRight()
                 print("Reached the start, moving to forward mode")
-                wait(60)
+                os.sleep(60)
                 return "forward"
             else
                 turtle.forward()
@@ -82,6 +106,7 @@ function move(mode)
     end
 end
 
+-- Main function to orchestrate farming
 function main()
     local mode = "forward"
     while true do
@@ -90,4 +115,5 @@ function main()
     end
 end
 
+-- Start the farming
 main()
