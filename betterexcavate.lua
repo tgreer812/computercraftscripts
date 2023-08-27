49b8fa4a35d41c7bd20a33655e2083dc5594c7bb
Tyler Greer <tgreer812@gmail.com>
2023-08-27 00:02:11 -0400

    Update betterexcavate.lua

--- betterexcavate.lua
+++ betterexcavate.lua
@@ -77,10 +77,7 @@ local function reset()
 end
  
 
-print("Better Excavate")
-print("https://github.com/na-stewart/Better-Excavate")
-print("------------------------------------")
-print("Excavation initiated, please monitor occasionally.")
+print("Excavation initiated...")
 for y = 1, depth do
     turtle.select(1)
     turtle.refuel()
