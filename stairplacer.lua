90c41902415bbed568fdd342de38aac798523eab
Tyler Greer <tgreer812@gmail.com>
2023-08-26 21:08:29 -0400

    Update stairplacer.lua

--- stairplacer.lua
+++ stairplacer.lua
@@ -1,54 +1,44 @@
--- Initialize variables
-local step_count = 0  -- Count of steps the turtle has moved
-local air_count = 0  -- Count of consecutive air blocks detected
-local stairSlot = 0  -- Slot containing stairs
+-- Stair creation script for a Turtle
+local stairSlot = 0
 
--- Function to find the stairs in the inventory
+-- Function to find stairs in the inventory
 function findStairs()
     for i = 1, 16 do
         local item = turtle.getItemDetail(i)
-        if item and item.name:find("stair") then
+        if item and item.name:find("stairs") then
             stairSlot = i
-            break
+            return true
         end
     end
+    return false
 end
 
--- Check for stairs in inventory
-findStairs()
-if stairSlot == 0 then
-    print("No stairs found in inventory. Exiting.")
+-- Function to detect if the turtle has reached the surface
+function atSurface()
+    local airCount = 0
+    for i = 1, 5 do
+        local success, data = turtle.inspectUp()
+        if not success then
+            airCount = airCount + 1
+        end
+    end
+    return airCount >= 4
+end
+
+-- Main execution starts here
+if not findStairs() then
+    print("No stairs in inventory. Exiting.")
     return
 end
 
--- Main loop
 while true do
-    -- Select the slot containing stairs
-    turtle.select(stairSlot)
-
-    if turtle.detect() then  -- If there's a block in front of the turtle
-        -- Reset the air count because a block is detected
-        air_count = 0  
-
-        -- Place the stair
-        turtle.place()
-
-        -- Move up one block and forward one block
-        turtle.up()
-        turtle.forward()
-
-        -- Increment the step_count
-        step_count = step_count + 1
+    if atSurface() then
+        print("Surface reached. Staircase completed.")
+        break
     else
-        -- An air block is detected, increment air_count
-        air_count = air_count + 1
-    end
-
-    -- Check for surface based on consecutive air blocks
-    if air_count >= 3 then
-        print("Surface detected. Stopping.")
-        break  -- Turtle has reached the surface, exit loop
+        turtle.up()
+        turtle.select(stairSlot)
+        turtle.placeDown()
+        turtle.forward()
     end
 end
-
-print("Staircase complete! Moved " .. step_count .. " steps.")
